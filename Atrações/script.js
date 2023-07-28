61d8bcfb16150bdf0b3d80c06714f1453bae1dbb
Lucas-Da-Gama <130692787+Lucas-Da-Gama@users.noreply.github.com>
2023-07-27 23:50:36 -0300

    JavaScript e um pingo de progresso

---
+++ Atrações/script.js
@@ -0,0 +1,17 @@
+var toboagua1 = document.getElementById("toboagua1");
+var toboagua2 = document.getElementById("toboagua2");
+var toboagua3 = document.getElementById("toboagua3");
+var toboagua4 = document.getElementById("toboagua4");
+var toboagua5 = document.getElementById("toboagua5");
+var ImagemCentral = document.getElementById("ImagemCentral")
+
+function trocarImagem1() {
+    ImagemCentral.src = "../Imagens/toboágua/toboágua.jpg"; // Substitua "nova_imagem.jpg" pelo caminho da nova imagem que deseja usar
+    ImagemCentral.alt = "toboagua1";     // Opcional: Altere o texto alternativo da imagem
+  }
+
+function trocarImagem2() {
+    ImagemCentral.src = "../Imagens/toboágua/toboágua2.jpg"; // Substitua "nova_imagem.jpg" pelo caminho da nova imagem que deseja usar
+    ImagemCentral.alt = "toboagua2";     // Opcional: Altere o texto alternativo da imagem
+}
+  
