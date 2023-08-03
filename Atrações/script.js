623a0c17069a8868aa15ace4eb52c6c14a9aa01e
Lucas-Da-Gama <goleirodovasco0@gmail.com>
2023-08-03 07:07:26 -0300

    São Pualo

--- Atrações/script.js
+++ Atrações/script.js
@@ -15,3 +15,49 @@ function trocarImagem2() {
     ImagemCentral.alt = "toboagua2";     // Opcional: Altere o texto alternativo da imagem
 }
   
+function trocarImagem3() {
+    ImagemCentral.src = "../Imagens/toboágua/toboágua3.jpg"; // Substitua "nova_imagem.jpg" pelo caminho da nova imagem que deseja usar
+    ImagemCentral.alt = "toboagua3";     // Opcional: Altere o texto alternativo da imagem
+}
+  
+function trocarImagem4() {
+    ImagemCentral.src = "../Imagens/toboágua/toboágua4.jpg"; // Substitua "nova_imagem.jpg" pelo caminho da nova imagem que deseja usar
+    ImagemCentral.alt = "toboagua4";     // Opcional: Altere o texto alternativo da imagem
+}
+  
+function trocarImagem5() {
+    ImagemCentral.src = "../Imagens/toboágua/toboágua5.jpg"; // Substitua "nova_imagem.jpg" pelo caminho da nova imagem que deseja usar
+    ImagemCentral.alt = "toboagua5";     // Opcional: Altere o texto alternativo da imagem
+}
+
+var Piscina1 = document.getElementById("Piscina1");
+var Piscina2 = document.getElementById("Piscina2");
+var Piscina3 = document.getElementById("Piscina3");
+var Piscina4 = document.getElementById("Piscina4");
+var Piscina5 = document.getElementById("Piscina5");
+var ImagemCentral2 = document.getElementById("ImagemCentral2")
+
+function trocarImagem6() {
+  ImagemCentral2.src = "../Imagens/Piscina/piscina1.jpg"
+  ImagemCentral2.alt = "piscina1"
+}
+
+function trocarImagem7() {
+  ImagemCentral2.src = "../Imagens/Piscina/piscina2.jpg"
+  ImagemCentral2.alt = "piscina2"
+}
+
+function trocarImagem8() {
+  ImagemCentral2.src = "../Imagens/Piscina/piscina3.jpg"
+  ImagemCentral2.alt = "piscina3"
+}
+
+function trocarImagem9() {
+  ImagemCentral2.src = "../Imagens/Piscina/piscina4.jpg"
+  ImagemCentral2.alt = "piscina4"
+}
+
+function trocarImagem10() {
+  ImagemCentral2.src = "../Imagens/Piscina/piscina5.jpg"
+  ImagemCentral2.alt = "piscina5"
+}
